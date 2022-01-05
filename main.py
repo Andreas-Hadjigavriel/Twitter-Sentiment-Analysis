347b6649e1d39b8db75b25359c64ecfe62b6d415
Andreas Hadjigavriel <74880624+AndreasHadjigavriel@users.noreply.github.com>
2022-01-05 19:01:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# Machine Learning Algorithms and Deep Learning Models we used in this Project:
-    # Logistic Regression
-    # XGBoost 
-    # Naive Bayes 
-    # SVM 
-    # Deep Learning Model: LSTM
+# Models we used in this Project:
+    # Machine Learning Algorithms:
+        # Logistic Regression
+        # XGBoost 
+        # Naive Bayes 
+        # SVM 
+    # Deep Learning Algorithms:
+        # LSTM
 
 # ---------------------------------------------------------------------------------------------------------- #
 
@@ -69,22 +71,18 @@ emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': '
 # Defining List containing all stopwords
 # Stopwords are the English words which does not add much meaning to a sentence. 
 # They can safely be ignored without sacrificing the meaning of the sentence. (eg: "the", "he", "have")
-stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
-             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
-             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
-             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from', 
-             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
-             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
-             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
-             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
-             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're',
-             's', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
-             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
-             'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 
-             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
-             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
-             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
-             "youve", 'your', 'yours', 'yourself', 'yourselves']
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an', 'and','any','are', 'as', 
+                'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between','both', 'by', 'can', 
+                'd', 'did', 'do', 'does', 'doing', 'down', 'during', 'each','few', 'for', 'from', 'further', 
+                'had', 'has', 'have', 'having', 'he', 'her', 'here', 'hers', 'herself', 'him', 'himself', 
+                'his', 'how', 'i', 'if', 'in', 'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+                'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once', 'only', 'or', 'other', 
+                'our', 'ours','ourselves', 'out', 'own', 're', 's', 'same', 'she', "shes", 'should', "shouldve",
+                'so', 'some', 'such', 't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+                'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 'through', 'to', 'too','under', 
+                'until', 'up', 've', 'very', 'was', 'we', 'were', 'what', 'when', 'where','which','while', 'who', 
+                'whom', 'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre", "youve", 'your', 'yours', 
+                'yourself', 'yourselves']
 
 # --------------------------------------- PREPROCESSING ------------------------------------------------- #
 
